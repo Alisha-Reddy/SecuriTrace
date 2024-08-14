1e902ba42fed5dd9fb96a30a68b6f93633f54afb
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-08-14 15:29:54 +0530

    pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import "@/styles/globals.css";
 
 // INTERNAL IMPORT
 import { TrackingProvider } from "@/Context/Tracking";
+import { Footer, NavBar } from "@/Components";
 export default function App({ Component, pageProps }) {
   return (
     <><TrackingProvider>
+      <NavBar />
       < Component {...pageProps} />
+      <Footer />
     </TrackingProvider>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,90 @@
-import React from 'react'
+
+
+// INTERNAL IMPORT
+import { useContext, useEffect, useState } from "react";
+import {
+  Table,
+  Form,
+  Service,
+  Profile,
+  CompleteShipment,
+  GetShipment,
+  StratShipment,
+  StartShipment,
+} from "../Components/index";
+import { TrackingContext } from "@/Context/Tracking";
 
 const index = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  const {
+    currentUser,
+    createShipment,
+    getAllShipment,
+    completeShipment,
+    getShipmnet,
+    stratShipment,
+    getShipmentsCount,
+  } = useContext(TrackingContext);
+
+  // STATE VARIABLE
+  const [createShipmentModel, setCraeteShipmentModel] = useState(false);
+  const [openProfile, setOpenProfile] = useState(false)
+  const [startModel, setStartModel] = useState(false)
+  const [completeModel, setCompleteModel] = useState(false);
+  const [getModel, setGetModel] = useState(false);
+
+  // DATA STATE VARIABLE
+  const [allShipmentsdata, setAllShipmentsdata] = useState();
+
+  useEffect(() => {
+    const getCampaignsData = getAllShipment();
+
+    return async () => {
+      const allData = await getCampaignsData;
+      setAllShipmentsdata(allData);
+    }
+  }, []);
+
+  return (
+    <>
+      <Service
+        setOpenProfile={setOpenProfile}
+        setCompleteModel={setCompleteModel}
+        setGetModel={setGetModel}
+        setStartModel={setStartModel}
+      />
+      <Table
+        setCraeteShipmentModel={setCraeteShipmentModel}
+        allShipmentsdata={allShipmentsdata}
+      />
+      <Form
+        createShipmentModel={createShipmentModel}
+        createShipment={createShipment}
+        setCraeteShipmentModel={setCraeteShipmentModel}
+      />
+      <Profile
+        openProfile={openProfile}
+        setOpenProfile={setOpenProfile}
+        currentUser={currentUser}
+        getShipmentsCount={getShipmentsCount}
+      />
+      <CompleteShipment
+        completeModel={completeModel}
+        setCompleteModel={setCompleteModel}
+        completeShipment={completeShipment}
+      />
+      <GetShipment
+        getModel={getModel}
+        setGetModel={setGetModel}
+        getShipmnet={getShipmnet}
+      />
+      <StartShipment
+        startModel={startModel}
+        setStartModel={setStartModel}
+        StartShipment={StartShipment}
+      />
+    </>
+  )
+
 }
 
-export default index
+export default index;
