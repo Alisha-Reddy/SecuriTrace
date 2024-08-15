060af5939d9ead983b481ba849979b00e0aa4e6f
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-08-15 15:15:04 +0530

    services

---
+++ Images/index.js
@@ -0,0 +1,17 @@
+import compShipment from "./compShipment.png"
+import getShipment from "./getShipment.png"
+import startShipment from "./startShipment.png"
+import userProfile from "./userProfile.png"
+import shipCount from "./shipCount.png"
+import send from "./send.png"
+
+const images = {
+    compShipment,
+    getShipment,
+    startShipment,
+    userProfile,
+    shipCount,
+    send,
+}
+
+export default images;
