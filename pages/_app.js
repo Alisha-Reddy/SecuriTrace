c5442fb738f1a59e0144bf08ae77308b47bd0387
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-08-19 12:07:14 +0530

    GetShipment

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
-import "@/styles/globals.css";
+import "@/styles/globals.css"
 
 // INTERNAL IMPORT
-import { TrackingProvider } from "@/Context/Tracking";
-import { Footer, NavBar } from "@/Components";
+import { TrackingProvider } from "@/Context/Tracking"
+import { Footer, NavBar } from "@/Components"
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <TrackingProvider>
-      <NavBar />
-      < Component {...pageProps} />
-      <Footer />
-    </TrackingProvider>
-    </>
-  );
+    return (
+        <>
+            <TrackingProvider>
+                <NavBar />
+                <Component {...pageProps} />
+                <Footer />
+            </TrackingProvider>
+        </>
+    )
 }
