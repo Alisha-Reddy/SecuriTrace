cfeac068f35121d40c33ccd80c3cedffd0688c6e
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-10-09 21:53:55 +0530

    .

---
+++ components/CompleteShipment.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react"
+import { Str1 } from "./index"
+import styles from "./Button.module.css"
+
+export default ({ completeModel, setCompleteModel, completeShipment }) => {
+    const [completeShip, setCompleteShip] = useState({
+        receiver: "",
+        index: "",
+    })
+
+    const changeStatus = async () => {
+        try {
+            await completeShipment(completeShip)
+            setCompleteShip({ receiver: "", index: "" })
+            setCompleteModel(false)
+            return true
+        } catch (error) {
+            console.log("Error completing shipment", error)
+            return false
+        }
+    }
+
+    return completeModel ? (
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div
+                className="fixed inset-0 w-full h-full bg-black opacity-40"
+                onClick={() => setCompleteModel(false)}
+            ></div>
+            <div className="flex items-center min-h-screen px-4 py-8">
+                <div
+                    className="relative w-full max-w-lg p-4 mx-auto  rounded-md shadow-lg"
+                    style={{ background: "#ccdaf8" }}
+                >
+                    <div className="flex justify-end">
+                        <button
+                            className="p-2 text-gray-400 rounded-md hover:bg-stone-50 hover:rounded-full "
+                            onClick={() => setCompleteModel(false)}
+                        >
+                            <Str1 />
+                        </button>
+                    </div>
+                    <div className="max-w-sm mx-auto py-3 space-y-3 text-center">
+                        <h4 className="text-lg font-medium text-gray-800">COMPLETE THE SHIPMENT</h4>
+                        <form onSubmit={(e) => e.preventDefault()}>
+                            <div className="relative mt-3">
+                                <input
+                                    type="text"
+                                    placeholder="Receiver"
+                                    className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border bg-white focus:border-black shadow-sm rounded-lg"
+                                    onChange={(e) =>
+                                        setCompleteShip({
+                                            ...completeShip,
+                                            receiver: e.target.value,
+                                        })
+                                    }
+                                />
+                            </div>
+                            <div className="relative mt-3">
+                                <input
+                                    type="number"
+                                    placeholder="ID"
+                                    className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border bg-white focus:border-black shadow-sm rounded-lg"
+                                    onChange={(e) =>
+                                        setCompleteShip({
+                                            ...completeShip,
+                                            index: e.target.value,
+                                        })
+                                    }
+                                />
+                            </div>
+                            <button
+                                onClick={async () => {
+                                    const success = await changeStatus()
+                                    if (success) {
+                                        window.location.reload()
+                                    }
+                                }}
+                                className={styles.styledButton}
+                                // className="block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-blue-950 hover:bg- active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600focus:ring-2"
+                                // style={{ background: "#000014" } }
+                            >
+                                {" "}
+                                Change Status
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ) : null
+}
---
+++ components/Form.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react"
+import { Close } from "./index"
+import styles from "./Button.module.css"
+
+export default ({ setCreateShipmentModel, createShipmentModel, createShipment }) => {
+    const [shipment, setShipment] = useState({
+        receiver: "",
+        pickupTime: "",
+        distance: "",
+        price: "",
+    })
+
+    const createItem = async () => {
+        try {
+            await createShipment(shipment)
+            setCreateShipmentModel(false)
+            return true
+        } catch (error) {
+            console.log("Something wet wrong with creating item")
+            return false
+        }
+    }
+
+    return createShipmentModel ? (
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div
+                className="fixed inset-0 w-full h-full bg-black opacity-40"
+                onClick={() => setCreateShipmentModel(false)}
+            ></div>
+            <div className="flex items-center min-h-screen px-4 py-8">
+                <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
+                    <div className="flex justify-end">
+                        <button
+                            className="p-2 text-gray-400 rounded-md hover:bg-gray-100"
+                            onClick={() => setCreateShipmentModel(false)}
+                        >
+                            <Close />
+                        </button>
+                    </div>
+                    <div className="max-w-sm mx-auto py-3 space-y-3 text-center">
+                        <h4 className="text-lg font-medium text-gray-800">
+                            Track product, Create Shipment
+                        </h4>
+                        <p className="text-[15px] text-gray-60">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+                            dolor sit amet consectetur adipisicing elit.
+                        </p>
+                        <form onSubmit={(e) => e.preventDefault()}>
+                            <div className="relative mt-3">
+                                <input
+                                    type="text"
+                                    placeholder="receiver"
+                                    className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                    onChange={(e) =>
+                                        setShipment({
+                                            ...shipment,
+                                            receiver: e.target.value,
+                                        })
+                                    }
+                                />
+                            </div>
+                            <div className="relative mt-3">
+                                <input
+                                    type="date"
+                                    placeholder="pickupTime"
+                                    className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent online-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                    onChange={(e) =>
+                                        setShipment({
+                                            ...shipment,
+                                            pickupTime: e.target.value,
+                                        })
+                                    }
+                                />
+                            </div>
+                            <div className="relative mt-3">
+                                <input
+                                    type="text"
+                                    placeholder="distance"
+                                    className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent online-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                    onChange={(e) =>
+                                        setShipment({
+                                            ...shipment,
+                                            distance: e.target.value,
+                                        })
+                                    }
+                                />
+                            </div>
+
+                            <div className="relative mt-3">
+                                <input
+                                    type="text"
+                                    placeholder="price"
+                                    className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent online-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                    onChange={(e) =>
+                                        setShipment({
+                                            ...shipment,
+                                            price: e.target.value,
+                                        })
+                                    }
+                                />
+                            </div>
+                            <button
+                                onClick={async () => {
+                                    const success = await createItem()
+                                    if (success) {
+                                        window.location.reload()
+                                    }
+                                }}
+                                className={styles.styledButton}
+                                // className="block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2"
+                            >
+                                Create Shipment
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ) : null
+}
