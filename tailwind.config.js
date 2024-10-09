5a2e7e83d3e9d387141b53ba5526a033f8a1bdd3
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-10-09 20:54:20 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ module.exports = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./images/**/*.{js,ts,jsx,tsx,mdx}",
-        "./_app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {
